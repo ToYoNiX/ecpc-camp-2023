4ae48c8ce74fa0fb7d934e312412b83e57a2091e
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-07-21 21:10:22 +0300

    </>

--- MUST_CPC/Training/ACPC_Training_2023/week3/team_contest/solutions/D - Longest Repeated Substring.cpp
+++ MUST_CPC/Training/ACPC_Training_2023/week3/team_contest/solutions/D - Longest Repeated Substring.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
-#include <algorithm>
+#include <map>
 #define ll long long
 #define fastio ios_base::sync_with_stdio(false); ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
 using namespace std;
 int main ()
 {
-    
+    fastio
+    string s;
+    cin >> s;
+
+    map <string, ll> mp;
+    int size = s.size();
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (i + j <= size)
+            {
+                mp[s.substr(i, j)]++;
+            }
+        }
+    }
+
+    ll ans = 0;
+    for (auto it = mp.begin(); it != mp.end(); it++)
+    {
+        if (it -> second < 2)
+        {
+            continue;
+        }
+
+        size = it -> first.size();
+        if (size > ans)
+        {
+            ans = size;
+        }
+    }
+
+    cout << ans << '\n';
+    return 0;
 }
