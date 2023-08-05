054014ccc75543ac95f33c93b9666abac9d32e65
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-08-05 23:32:15 +0300

    </>

--- Others/Codeforces/144A_Arrival of the General.cpp
+++ Others/Codeforces/144A_Arrival of the General.cpp
@@ -5,38 +5,6 @@ using namespace std;
 int main ()
 {
     fastio
-    int n;
-    cin >> n;
-    int arr[n];
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
-
-    int movements = 0, swapc = -1;
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (arr[i] < arr[i + 1])
-        {
-            swap(arr[i], arr[i + 1]);
-            movements++;
-            swapc++;
-        }
-
-        if (swapc != 0 && i == n - 2)
-        {
-            i = -1;
-            swapc = 0;
-        }
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << ' ';
-    }
-
-    cout << movements << '\n';
+    
     return 0;
 }
