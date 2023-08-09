62ace640f54a804048830c1d6e4387b73f7404fb
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-08-09 16:51:31 +0300

    </>

--- Others/Codeforces/144A_Arrival of the General.cpp
+++ Others/Codeforces/144A_Arrival of the General.cpp
@@ -5,6 +5,25 @@ using namespace std;
 int main ()
 {
     fastio
-    
+    ll n, a;
+    cin >> n >> a;
+    ll min = a, min_idx, max = a, max_idx;
+    for (int i = 1; i < n; i++)
+    {
+        cin >> a;
+        if (a > max)
+        {
+            max = a;
+            max_idx = i;
+        }
+        
+        if (a <= min)
+        {
+            min = a;
+            min_idx = i;
+        }
+    }
+
+    cout << max_idx + (n - (min_idx + 1)) - (min_idx < max_idx ? 1 : 0) << '\n';
     return 0;
 }
