da2af24e4f2ab57cb0a58392ffe80eb48b2328d8
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-08-13 04:28:43 +0300

    </>

---
+++ MUST_CPC/Training/ACPC_Training_2023/week1/team_contest/solutions/E. Digits Sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false); ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+using namespace std;
+int main ()
+{
+    string s;
+    cin >> s;
+
+    ll counter = 0;
+    while (s.size() != 1)
+    {
+        ll sum = 0;
+        for (ll i = 0; i < s.size(); i++)
+        {
+            sum += s[i] - '0';
+        }
+
+        s = to_string(sum);
+        counter++;
+    }
+
+    cout << counter << '\n';
+    return 0;
+}
