90f06540a0c33cef25879cd041d9a6226eada99a
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2023-08-13 04:12:39 +0300

    </>

---
+++ MUST_CPC/Training/ACPC_Training_2023/week1/team_contest/solutions/D. Find Numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#define ll long long
+#define fastio ios_base::sync_with_stdio(false); ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+using namespace std;
+int main ()
+{
+    fastio
+    ll s, g;
+    cin >> s >> g;
+
+    if (__gcd(s - g, g) == g)
+    {
+        cout << s - g << ' ' << g << '\n';
+    }
+    else
+    {
+        cout << -1 << '\n';
+    }
+
+    return 0;
+}
